c702e2eea2b514647213d007ce8a7d9a9dff24e3
鴇峰 朔華 <sakuhana@tsuitachi.net>
2024-04-18 11:29:31 +0900

    Hello world

--- src/main/java/net/tsuitachi/springbootsample/SpringbootSampleApplication.java
+++ src/main/java/net/tsuitachi/springbootsample/SpringbootSampleApplication.java
@@ -2,12 +2,26 @@ package net.tsuitachi.springbootsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class SpringbootSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSampleApplication.class, args);
+    }
+
+}
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, World!";
     }
 
 }
