5e91618df1e4a8f8b0bfe864f4e4237d98b3e88a
鴇峰 朔華 <sakuhana@tsuitachi.net>
2024-04-18 10:38:59 +0900

    first

---
+++ src/test/java/net/tsuitachi/springbootsample/SpringbootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tsuitachi.springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
